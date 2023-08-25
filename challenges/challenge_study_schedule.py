cf43502596f766ef28f338f493e8e8f800b47104
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-08-25 03:04:42 -0300

    REQ 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    time_valid = 0
+    if not target_time:
+        return None
+    for start, out in permanence_period:
+        if not isinstance(start, int) or not isinstance(out, int):
+            return None
+        if start <= target_time <= out:
+            time_valid += 1
+    return time_valid
